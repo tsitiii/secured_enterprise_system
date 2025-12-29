8eec15ceea94f48eb46bd4f6b2afdf90009b0fb0
tsitiii <ziontaa9@gmail.com>
2025-12-29 20:54:30 +0300

    hosts

--- enterprise_system/enterprise_system/settings.py
+++ enterprise_system/enterprise_system/settings.py
@@ -16,10 +16,7 @@ RECAPTCHA_SECRET_KEY = os.environ.get('RECAPTCHA_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost'
-                    '.render.com',
-                    'render.com',
-                ]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
