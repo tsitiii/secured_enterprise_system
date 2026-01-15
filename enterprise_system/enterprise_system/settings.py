473b45ce8baf69920595d180ac90ec665282587f
Tsiyon Gashaw <130053047+tsitiii@users.noreply.github.com>
2026-01-15 23:51:37 +0300

    Update settings.py

--- enterprise_system/enterprise_system/settings.py
+++ enterprise_system/enterprise_system/settings.py
@@ -6,7 +6,8 @@ load_dotenv()
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like 
+BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
