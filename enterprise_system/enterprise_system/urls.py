612c84c606e023bf97cf36ab1013f6dff0658400
Tsiyon Gashaw <130053047+tsitiii@users.noreply.github.com>
2026-01-15 23:52:54 +0300

    Update urls.py

--- enterprise_system/enterprise_system/urls.py
+++ enterprise_system/enterprise_system/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
-
+#URLs
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('accounts.urls')),
